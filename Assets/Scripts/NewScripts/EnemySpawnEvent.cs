a0e97ae996ad58eac1c4759b3e0a99e99f608d9d
Jaype27 <jaypee.0927@gmail.com>
2019-10-29 23:54:20 -0400

    wall tile assets, bridge tile assets

---
+++ Assets/Scripts/NewScripts/EnemySpawnEvent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnEvent : MonoBehaviour {
+
+	ObjectPooler _objectPooler;
+	public int _enemyCount;
+	public float _startWait;
+	public float _spawnWait;
+	public Transform _spawnPoint;
+
+	void Start() {
+		_objectPooler = ObjectPooler.Instance;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)	{
+		if(other.gameObject.tag == "Player") {
+			StartCoroutine(SpawnEnemies());	
+		}
+	}
+
+	IEnumerator SpawnEnemies() {
+		yield return new WaitForSeconds(_startWait);
+		for (int i = 0; i < _enemyCount; i++) {
+			_objectPooler.SpawnFromPool("enemy", _spawnPoint.position, _spawnPoint.rotation);
+			yield return new WaitForSeconds(_spawnWait);
+		}
+
+		this.gameObject.SetActive(false);
+	}
+}
---
+++ Assets/Scripts/NewScripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public PlayerController _player;
+	public Transform _spawnPoint;
+	public float _spawnWait = 2.0f;
+
+	public static GameManager Instance { get { return _instance; } }
+	private static GameManager _instance = null;
+
+	void Awake() {
+		if(_instance != null && _instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			_instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+		RespawnPlayer();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void RespawnPlayer() {
+		StartCoroutine(SpawnPlayer());
+	}
+
+	private IEnumerator SpawnPlayer() {
+		yield return new  WaitForSeconds(_spawnWait);
+		_player.transform.position = _spawnPoint.position;
+		_player.gameObject.SetActive(true);
+		_player._knockbackCount = 0f;
+	}
+}
